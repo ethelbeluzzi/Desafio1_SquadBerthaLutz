a94bd70045b24a3a0c3ef9619888cea0f08f00d1
Hyngrid Souza <100658131+Hyngras@users.noreply.github.com>
2024-08-04 10:20:12 -0300

    Create 2_IdadeAtual.py

---
+++ Exercicio 02/2_IdadeAtual.py
@@ -0,0 +1,5 @@
+# Peça ao usuário para informar o ano de nascimento. Em seguida, calcule e imprima a idade atual.
+
+ano = int(input("Qual o seu ano de Nascimento?"))
+idade_atual = ano - 2024
+print("Sua Idade Atual é:", idade_atual)
