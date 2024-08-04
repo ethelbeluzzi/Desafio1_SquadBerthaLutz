38c8e757ce51cb3e566e764595a9a5bf83236bbd
Hyngrid Souza <100658131+Hyngras@users.noreply.github.com>
2024-08-04 11:48:00 -0300

    Update exercicioresolvido.py

--- exercicio 02/exercicioresolvido.py
+++ exercicio 02/exercicioresolvido.py
@@ -1,5 +1,5 @@
 # Peça ao usuário para informar o ano de nascimento. Em seguida, calcule e imprima a idade atual.
 
 ano = int(input("Qual o seu ano de Nascimento?"))
-idade_atual = ano - 2024
+idade_atual = 2024 - ano
 print("Sua Idade Atual é:", idade_atual)
